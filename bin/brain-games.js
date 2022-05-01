93a798b7fa473f7c81df3f73c47c2d1d910924f8
AksenovKirill <aksenovkirill@yandex.ru>
2022-05-01 23:52:58 +0300

    Second step: add Makefile, install make -g

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* import {questionPlayer} from '../src/cli.js';
-questionPlayer(); */
-console.log("Welcome to the Brain Games");
+questionPlayer();  */
+console.log('Welcom to hte Brain-Games');
