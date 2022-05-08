2ad1c5d72a841abf00ffab60bcd770831a8125bb
AksenovKirill <aksenovkirill@yandex.ru>
2022-05-08 13:41:53 +0300

    fix bugs and test

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,18 +10,19 @@ const RULE_GAME = 'What number is missing in the progression?';
 
 const getDataForGame = () => {
   const number = getRandomIntenger(MIN_NUMBER, MAX_NUMBER);
-  const answer = getRandomIntenger(MIN_NUMBER, MAX_NUMBER);
+  const step = getRandomIntenger(MIN_NUMBER, MAX_NUMBER);
   const position = getRandomIntenger(MIN_POSITION, MAX_POSITION);
   const progressionLength = getRandomIntenger(MIN_LENGTH, MAX_LENGTH);
   const progression = [number];
   let result = number;
 
   for (let i = 1; i <= progressionLength; i++) {
-    result += answer;
+    result += step;
     progression.push(result);
   }
 
-  progression.splice(position, 1, '..');
+  const answer = progression.splice(getRandomIntenger(0, position), 1, '..');
+  console.log('OTVET', answer)
   const question = `Question: ${progression.join(' ')}`;
   return [question, answer];
 };
