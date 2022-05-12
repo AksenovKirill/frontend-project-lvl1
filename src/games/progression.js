47bcbf6204a0dc9a92017476715a103f36f10549
AksenovKirill <aksenovkirill@yandex.ru>
2022-05-13 00:17:04 +0300

    fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,7 +3,7 @@ import { MIN_NUMBER, MAX_NUMBER, GAME_RULES } from '../const.js';
 import playGame from '../play-game.js';
 
 const MIN_POSITION = 1;
-const MAX_POSITION = 10;
+const MAX_POSITION = 9;
 const MIN_LENGTH = 5;
 const MAX_LENGTH = 10;
 
@@ -23,8 +23,7 @@ const getDataForGame = () => {
     },
   );
 
-  const answer = progression.splice(getRandomIntenger(0, position - 1), 1, '..');
-  console.log(answer);
+  const answer = progression.splice(getRandomIntenger(0, position), 1, '..');
   const question = `Question: ${progression.join(' ')}`;
   return [question, answer];
 };
